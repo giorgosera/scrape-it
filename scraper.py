2f4c3bb17a4bf8f91d73ae4c75b7d305800606ff
George Eracleous <basketballcy@gmail.com>
2012-09-13 10:57:07 +0100

    Scraping more contents

--- scraper.py
+++ scraper.py
@@ -1,29 +1,32 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
+import datetime
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
-soup = BeautifulSoup(html_doc)
-
-
-html = urllib2.urlopen("http://www.groupon.co.uk/deals/national-deal/bhs/10353038").read()
-
+html = urllib2.urlopen("http://www.groupon.co.uk/deals/national-deal/26000bijouxcom/10250996").read()
 soup = BeautifulSoup(html)
 
-price_tag = soup.find("span", {"class": "price"})
-amount_tag = price_tag.find("span", {"class": "noWrap"})
+#Get deal's title
+title_tag = soup.find("div", {"id": "contentDealTitle"})
+title = title_tag.h1.a.string.strip()
 
-print amount_tag.string.rpartition(';')[2]
+#Get price
+price_tag = soup.find("span", {"class": "price"})
+price_tag = price_tag.find("span", {"class": "noWrap"})
+price = price_tag.string.rpartition(';')[2]
+
+#Get quantity sold
+amount_sold_tag = soup.find("div", {"class": "soldAmount"})
+amount_sold_tag = amount_sold_tag.find("span", {"id": "jDealSoldAmount"})
+amount_sold = amount_sold_tag.string.rpartition(';')[2]
+
+#Record date
+date = datetime.datetime.now()
+
+print 'Scraping Groupon biatch!'
+print '==========================='
+print '-->Item: ', title
+print '-->Price: ', price
+print '-->Quantity sold: ', amount_sold
+print '-->Datetime: ', date
+print '==========================='
 
